b33310b0737ecd10032fbc5a247debee90443ac3
martoccia.lorenzo <martoccia.lorenzo@TAVE-PC22.OSDBSESTO.local>
2020-11-10 13:18:35 +0100

    add new print

--- src/prova/git/hub/ProvaGitHub.java
+++ src/prova/git/hub/ProvaGitHub.java
@@ -15,9 +15,10 @@ public class ProvaGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int i=2;
+        int i = 2;
         while(true){
-            System.out.println("ciao: "+i);
+            System.out.println("ciao: " + i);
+            System.err.println("ciao di nuovo");
             i = i * i;
         }
     }
