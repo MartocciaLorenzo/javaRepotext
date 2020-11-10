e571cb0bdb88275c2776bd7580a1630d57fb121a
martoccia.lorenzo <martoccia.lorenzo@TAVE-PC22.OSDBSESTO.local>
2020-11-10 13:02:58 +0100

    add endless loop

---
+++ src/prova/git/hub/ProvaGitHub.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prova.git.hub;
+
+/**
+ *
+ * @author martoccia.lorenzo
+ */
+public class ProvaGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double i=2;
+        while(true){
+            System.out.println("ciao: "+i);
+            i = i * i;
+        }
+    }
+    
+}
